6aca70aea9c5ceed4877ece204201f074a116c8a
Alex Li <ali@cellossoftware.com>
2018-06-05 10:23:22 +1000

    pinscala.c34 wip

--- src/main/scala/pinscala/c34/TestApp.scala
+++ src/main/scala/pinscala/c34/TestApp.scala
@@ -1,16 +1,39 @@
 package pinscala.c34
 
 /* KeyNotes:
- * 	- 
- * 
+ * 	- 'MainFrame'   (closing it will shut down the GUI application)
+ * 		1. title bar 
+ *		2. container: add/remove components/contents 
+ * 	- 'Event Handling'   (publish/subscribe approach)
+ * 		1. 'trait Event' is scala way of 'publishing events'
  * */
 
 import scala.swing._
+import scala.swing.event.ButtonClicked
 
 object TestApp extends SimpleSwingApplication {
   
   def top = new MainFrame {
     title = "== c34: GUI Programming =="
-    contents = new Button { text = "Click me" }
+    
+    val btn = new Button { text = "Click me" }
+    val lab = new Label { text = "No button clicks registered" }
+    
+    contents = new BoxPanel(Orientation.Vertical) {
+      contents += btn
+      contents += lab
+      border = Swing.EmptyBorder(30, 30, 10, 30)
+    }
+    
+    listenTo(btn)
+    var clckCount = 0
+    reactions += {
+      case ButtonClicked(b) => {
+        clckCount += 1
+        lab.text = "clicks: " + clckCount
+      }
+    }
   }
+  
+  
 }
